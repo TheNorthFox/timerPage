6785359056d822ca6f87b792b469479c23d83fd5
Kocha <kocha@users-imac.home>
2020-11-22 15:15:45 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { timer } from 'rxjs';
+
+
+const counter = timer(0, 1000);
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'assessment2';
+
+  public clockCounter;
+  
+  ngOnInit() {
+    counter.subscribe(() => {
+      this.timeCounter();
+    });
+  }
+
+  timeCounter() {
+    let date = new Date();
+    
+
+    let second:number | string = date.getSeconds();
+    let minute:number | string = date.getMinutes();
+    let hour:number | string = date.getHours();
+    let days:number | string = date.getDay();
+    if (second < 10) {
+      second = '0' + second;
+    }
+    if (minute < 0) {
+      minute = '0' + minute
+    }
+    if(days < 10 ){
+      days = '0' + days
+    }
+    this.clockCounter = days + " : " + hour + " : " + minute + " : " + second;
+  }
 }

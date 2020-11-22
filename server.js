5898a89f909a34ab0df3417c69e9528ad7d11196
Kocha <kocha@users-imac.home>
2020-11-22 15:00:25 -0600

    first commit

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
-//Install express server
+
 const express = require('express');
-const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/{{assessment2}}'));
+app.use(express.static('./dist/assessment2'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname,'/dist/{{assessment2}}/index.html'));
+app.get('/*', function (req, res) {
+  res.sendFile('index.html', { root: 'dist/assessment2' }
+  );
 });
 
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
+console.log(`Running on port ${process.env.PORT || 8080}`)
